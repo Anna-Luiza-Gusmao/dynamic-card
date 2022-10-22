e1de5eccb77ab5506ea2600a71fd14afaee33a66
Anna-Luiza-Gusmao <luizasgusmao@hotmail.com>
2022-10-21 23:07:43 -0300

    add card apple

--- src/main.js
+++ src/main.js
@@ -7,17 +7,22 @@ const cartaoBgColor01 = document.querySelector(
 const cartaoBgColor02 = document.querySelector(
   ".cc-bg svg > g g:nth-child(2) path"
 )
+/*const cartaoBgColor03 = document.querySelector(
+  ".cc-bg svg > g g:nth-child(3) path"
+)*/
 const cartaoLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
-    default: ["black", "gray"],
+    apple: ["#F73A67", "#0D0B2A"],
+    default: ["black", "gray"]
   }
 
   cartaoBgColor01.setAttribute("fill", colors[type][0])
   cartaoBgColor02.setAttribute("fill", colors[type][1])
+  //cartaoBgColor03.setAttribute("fill", colors[type][2])
   cartaoLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
@@ -60,6 +65,11 @@ const cardNumberPattern = {
       mask: "0000 0000 0000 0000",
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^3\d{0,15}/,
+      cardtype: "apple"
     },
     {
       mask: "0000 0000 0000 0000",
